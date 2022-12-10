6de73fc3eb1e204fbdea7529ae51a5eb2fc4ca61
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2022-12-10 19:55:27 +0200

    #20 | Add users get api endpoint

---
+++ src/pages/api/users/get.ts
@@ -0,0 +1,25 @@
+import db from 'lib/db';
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {getSession} from 'next-auth/react';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getSession({req});
+
+  if (!session) {
+    return res.status(401).json({error: 'Not authenticated'});
+  }
+
+  const user = await db.user.findFirst({
+    where: {
+      id: session.user.id,
+    },
+  });
+
+  if (!user) {
+    return res.status(404).json({error: 'User not found'});
+  }
+
+  res.status(200).json(user);
+};
+
+export default handler;
