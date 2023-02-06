a56a9acb7ed7c2cab4711c602b99af3c8da1b420
Martin Velkov <72291444+MartsTech@users.noreply.github.com>
2023-02-06 23:01:27 +0200

    #114 | Add privacy policy and terms of use

---
+++ apps/web/src/pages/privacy_policy.tsx
@@ -0,0 +1,72 @@
+import type {NextPage} from 'next';
+
+const Privacy: NextPage = () => {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>
+        This privacy policy explains how Invenr collects, uses, and protects the
+        personal information of our users. We are committed to ensuring the
+        privacy of our users and will only use information collected in
+        accordance with this policy.
+      </p>
+
+      <h2>Information Collection and Use</h2>
+      <p>
+        We collect information from our users in order to provide them with a
+        better experience on our web application. This information may include:
+      </p>
+      <ul>
+        <li>Contact information, such as name and email address</li>
+        <li>Calendar information, such as events and reminders</li>
+      </ul>
+      <p>
+        We use this information to provide and improve our services, to
+        communicate with you, and to fulfill our obligations under this privacy
+        policy and our terms of use.
+      </p>
+
+      <h2>Information Sharing and Disclosure</h2>
+      <p>
+        We do not sell, rent, or share your personal information with third
+        parties for their marketing purposes without your explicit consent. We
+        may share your information with third parties for the following reasons:
+      </p>
+      <ul>
+        <li>To provide our services and improve our web application</li>
+        <li>
+          To comply with legal requirements, such as a subpoena or court order
+        </li>
+        <li>
+          To protect the rights, property, or safety of Invenr, our users, or
+          others
+        </li>
+      </ul>
+
+      <h2>Data Security</h2>
+      <p>
+        We take the security of your information seriously and implement
+        appropriate technical and organizational measures to protect it from
+        unauthorized access, alteration, disclosure, or destruction. Despite
+        these measures, we cannot guarantee the security of your information and
+        will not be liable for any unauthorized access to it.
+      </p>
+
+      <h2>Changes to This Privacy Policy</h2>
+      <p>
+        We may update this privacy policy from time to time to reflect changes
+        in our practices or services. If we make any changes, we will update the
+        “Last Updated” date at the top of this page. We encourage you to review
+        this privacy policy regularly to stay informed of our privacy practices.
+      </p>
+
+      <h2>Contact Us</h2>
+      <p>
+        If you have any questions or concerns about this privacy policy or the
+        information we collect, please contact us at invenr.martstech.com
+      </p>
+    </div>
+  );
+};
+
+export default Privacy;
---
+++ apps/web/src/pages/terms_of_use.tsx
@@ -0,0 +1,109 @@
+import type {NextPage} from 'next';
+
+const TermsOfUse: NextPage = () => {
+  return (
+    <div>
+      <h1>Terms of Use</h1>
+      <p>
+        These terms of use (“Terms”) govern your use of the Invenr web
+        application (the “Service”). By accessing or using the Service, you
+        agree to be bound by these Terms and our Privacy Policy. If you do not
+        agree to these Terms, you may not use the Service.
+      </p>
+
+      <h2>Use of Service</h2>
+      <p>
+        You may use the Service only for lawful purposes and in accordance with
+        these Terms. You agree not to:
+      </p>
+      <ul>
+        <li>
+          Use the Service to upload, transmit, or otherwise distribute any
+          content that is illegal, harmful, threatening, abusive, harassing,
+          defamatory, vulgar, obscene, or otherwise objectionable
+        </li>
+        <li>
+          Use the Service to upload, transmit, or otherwise distribute any
+          content that infringes any patent, trademark, trade secret, copyright,
+          or other proprietary rights of any party
+        </li>
+        <li>
+          Use the Service to upload, transmit, or otherwise distribute any
+          unsolicited or unauthorized advertising, promotional materials, junk
+          mail, spam, chain letters, or any other form of solicitation
+        </li>
+        <li>
+          Interfere with or disrupt the Service or servers or networks connected
+          to the Service, or disobey any requirements, procedures, policies, or
+          regulations of networks connected to the Service
+        </li>
+      </ul>
+
+      <h2>Modification of Service and Terms</h2>
+      <p>
+        We reserve the right, at any time and from time to time, to modify or
+        discontinue, temporarily or permanently, the Service (or any part
+        thereof) with or without notice. We also reserve the right to modify
+        these Terms at any time, and your continued use of the Service after any
+        such modification constitutes your acceptance of the new Terms.
+      </p>
+
+      <h2>Termination</h2>
+      <p>
+        We may, in our sole discretion, at any time terminate or suspend all or
+        a portion of the Service, or your use of all or a portion of the
+        Service, with or without notice and with or without cause.
+      </p>
+
+      <h2>Disclaimer of Warranties</h2>
+      <p>
+        The Service is provided on an “as is” and “as available” basis. We make
+        no representations or warranties of any kind, express or implied, as to
+        the operation of the Service or the information, content, materials, or
+        products included on the Service. You agree that your use of the Service
+        is at your sole risk.
+      </p>
+
+      <h2>Limitation of Liability</h2>
+      <p>
+        In no event shall Invenr be liable for any direct, indirect, incidental,
+        special, punitive, or consequential damages arising out of, or in any
+        way connected with, your use of the Service.
+      </p>
+
+      <h2>Indemnification</h2>
+      <p>
+        You agree to indemnify, defend, and hold harmless Invenr, its officers,
+        directors, employees, agents, licensors, and suppliers (collectively the
+        “Service Providers”) from and against all losses, expenses, damages, and
+        costs, including reasonable attorneys' fees, resulting from any
+        violation of these Terms or any activity related to your account
+        (including negligent or wrongful conduct) by you or any other person
+        accessing the Service using your account.
+      </p>
+
+      <h2>Entire Agreement</h2>
+      <p>
+        These Terms, together with our Privacy Policy, constitutes the entire
+        agreement between you and Invenr with respect to the use of the Service.
+      </p>
+
+      <h2>Changes to Terms of Use</h2>
+      <p>
+        We reserve the right, at our sole discretion, to modify or replace these
+        Terms at any time. If a revision is material, we will try to provide at
+        least 30 (change this) days' notice prior to any new terms taking
+        effect. What constitutes a material change will be determined at our
+        sole discretion.
+      </p>
+
+      <h2>Contact Information</h2>
+      <p>
+        If you have any questions about these Terms, please contact us at
+        invenr.martstech.com
+      </p>
+    </div>
+  );
+};
+
+export default TermsOfUse;
